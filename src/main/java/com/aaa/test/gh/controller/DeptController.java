4f6ff194748ab63c627338620533ec09eabb0072
qy88xfz <767299286@qq.com>
2018-11-29 09:33:31 +0800

    Initial commit

---
+++ src/main/java/com/aaa/test/gh/controller/DeptController.java
@@ -0,0 +1,14 @@
+package com.aaa.test.gh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:xiefuzhi
+ * createTime:2018-11-29 09:07
+ */
+
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("测试github提交111");
+    }
+}
