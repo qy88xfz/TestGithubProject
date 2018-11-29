bb0094592fe8ef0df2998045b85b753ab7eccbe0
qy88xfz <767299286@qq.com>
2018-11-29 10:48:30 +0800

    合并分支

--- src/main/java/com/aaa/test/gh/controller/DeptController.java
+++ src/main/java/com/aaa/test/gh/controller/DeptController.java
@@ -10,5 +10,6 @@ package com.aaa.test.gh.controller;
 public class DeptController {
     public static void main(String[] args) {
         System.out.println("测试github提交111");
+        System.out.println("测试合并分支111");
     }
 }
