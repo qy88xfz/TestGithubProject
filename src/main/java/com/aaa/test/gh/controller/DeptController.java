0a5a0c234d41867ec98bb0d686f634c4d6797531
qy88xfz <767299286@qq.com>
2018-11-29 14:34:23 +0800

    我想要冲突

--- src/main/java/com/aaa/test/gh/controller/DeptController.java
+++ src/main/java/com/aaa/test/gh/controller/DeptController.java
@@ -12,7 +12,7 @@ public class DeptController {
         System.out.println("测试github提交111");
         System.out.println("测试合并分支111");
         System.out.println("6666");
-        System.out.println("666666666");
+        System.out.println("66666");
         System.out.println("9999");
     }
 }
